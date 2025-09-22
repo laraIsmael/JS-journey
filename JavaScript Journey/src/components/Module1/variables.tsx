5463b90bac67459112ea602bff357658bc3364ab
Lara Ismael <lara8ismael@gmail.com>
2025-09-22 13:41:22 -0700

    variable almost done

--- JavaScript Journey/src/components/Module1/variables.tsx
+++ JavaScript Journey/src/components/Module1/variables.tsx
@@ -2,14 +2,42 @@ import NavigationButtons from "../NavigatorButtons";
 import CodeBlock from "../codeBlock";
 
 export default function Variables() {
-  const letTitle = "creating a variable using the keyword LET";
-  const letDescriptio = " this is a little description of how let works";
-  const letCode = `
-    let hello = "Hello, "
-    let name = "JavaScript"
+  const letTitle = "Declaring a variable";
+  const letDescriptio =
+    "line 1 declaring a variable hello. \n line 2 adding a value to that ";
+  const letCode = `  // declaring a variable:
+  let hello;
+  
+  // adding a value to the variable we just created:
+  hello = "Hello,";
 
-    console.log(hello+name)
+  // both steps can be done at once:
+  let name = "JavaScript";
+
+  // now we can access that values by using the variable name, 
+  console.log(hello, name)
   `;
+
+  const constTitle = "Const or Let";
+  const constDescriptio =
+    "When declaring a variable you can use the keyword let or const. The difference is that let can change its values where const (constant) as the name suggests can not. \n If you want to make sure a variable will not change naming it const will ensure that is the case and will tell other people reading your code that as well. Let's look at some examples:";
+  const constCode = `  // declating a varibale with the keyword const. As much as I want to not age my birhtday eyar will continue to be the same, so let's create a variable that stores my day of birth:
+  const myBirthday = "01/25/1988"
+
+  // if we try cnaging that value we will get an error.
+  myBirthday = 01/25/2000;
+
+  // If you run the code above ypou will see we get an error. Try it out!`;
+
+  const taskTitle = "Now it's your turn to try:";
+  const taskDescriptio =
+    '1. Declare two variables: admin and name. \n 2. Assign the value "JavaScript" to the variable name. \n 3. Copy the value from name to admin. \n ';
+  const taskCode = `  //write your code starting on line 2.
+  
+  
+  // this console.log will print your answers. We want to see that both name and admin have the value of JavaScript
+  console.log(name);
+  console.log(name, admin);`;
 
   return (
     <div className="min-h-screen bg-gray-900 text-gray-100 flex flex-col items-center p-6 sm:p-10 transition-all duration-300">
@@ -27,17 +55,33 @@ export default function Variables() {
             <br />
             It gets stored and becomes accessible through out the program.
             <br />
-            - There are more details regarding the accessibility of a variables,
-            we will talk about that later.
+            <p className="text-gray-400 text-sm mt-4 italic">
+              - There are more details regarding the accessibility of a
+              variables, we will talk about that later.
+            </p>
             <br />
-            This is a little outside of the scode pf this course but we are
-            going to use console.log to print the results of what But for now
-            lets dive into VARIASBLES:
+            To create or declare a variable in JavaScript we use the keyword{" "}
+            <code className="bg-gray-900 px-2 py-1 rounded text-green-400 font-mono">
+              let
+            </code>{" "}
+            after that we add a name that will be our reference to that variable
+            that we can access at any point. Let's look at an example to make
+            things more clear.
           </p>
           <CodeBlock
             title={letTitle}
             description={letDescriptio}
             initialCode={letCode}
+          />
+          <CodeBlock
+            title={constTitle}
+            description={constDescriptio}
+            initialCode={constCode}
+          />
+          <CodeBlock
+            title={taskTitle}
+            description={taskDescriptio}
+            initialCode={taskCode}
           />
         </div>
       </div>
