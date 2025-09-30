4ca6bf7b133bc783df9a7fe4667e8b5be43fb275
Lara Ismael <lara8ismael@gmail.com>
2025-09-30 16:17:41 -0700

    working on data-types: numbers

--- JavaScript Journey/src/components/Module1/data-types.tsx
+++ JavaScript Journey/src/components/Module1/data-types.tsx
@@ -31,18 +31,15 @@ export default function DataTypes() {
         <strong>data type</strong> is what’s actually inside the drawer—socks,
         snacks, or maybe that one tangle of mystery cables we all own. 🧦🍪🔌
       </p>
-
       <code className="bg-gray-900 px-2 py-1 rounded text-purple-400 font-mono">
         [Number, BigInt, String, Boolean, null, undefined, Symbol, Object]
       </code>
-
       <p className="text-gray-400 text-lg sm:text-xl text-left mb-2 mt-2">
         The first seven are called <strong>primitive</strong> data types, and
         then there’s <code>Object</code>—the overachiever that refuses to fit
         neatly into the same box. It’s like that one drawer where you keep
         “miscellaneous stuff.”
       </p>
-
       <p className="text-gray-400 text-lg sm:text-xl text-left mb-6">
         JavaScript is{" "}
         <code className="bg-gray-900 px-2 py-1 rounded text-purple-400 font-mono text-base sm:text-lg">
@@ -52,34 +49,76 @@ export default function DataTypes() {
         and next thing you know, it’s full of cookies. The drawer doesn’t mind.
         🍪
       </p>
-
       <code className="bg-gray-900 px-2 py-1 rounded text-purple-400 font-mono block whitespace-pre">
         let greeting = "Hello!";
         {"\n"}greeting = 123;
       </code>
-
       <p className="text-gray-400 text-lg sm:text-xl text-left mb-6">
         See that? Your string just shapeshifted into a number. It’s flexible and
         sometimes magical, but also a little reckless.
       </p>
-
       <p className="text-gray-400 text-lg sm:text-xl text-left mb-6 italic">
         🔒 Pro tip: If you don’t love the idea of your drawers constantly
         swapping contents, TypeScript comes to the rescue. It adds labels that
         say “This drawer only holds socks” (or numbers, or strings) and makes
         sure you stick to it. Fewer surprises, fewer bugs, more peace of mind.
       </p>
-
-      {/* --- String Section --- */}
-
       {/* --- Number Section --- */}
+      <h3 className="text-4xl sm:text-3xl font-extrabold text-purple-400 mb-6 text-cleft">
+        Numbers
+      </h3>
+      <code className="bg-gray-900 px-2 py-1 my-4 rounded text-purple-400 font-mono block whitespace-pre">
+        let int = 123;
+        {"\n"}let float = 12.3;
+      </code>
+      <p className="text-gray-400 text-lg sm:text-xl text-left mb-6">
+        As you can imagine this deta type represents{" "}
+        <code className="bg-gray-900 px-2 py-1 mx-2 rounded text-purple-400 font-mono text-base sm:text-lg">
+          interger
+        </code>{" "}
+        but it also represents {"  "}
+        <code className="bg-gray-900 px-2 py-1 mx-2 rounded text-purple-400 font-mono text-base sm:text-lg">
+          floating point
+        </code>{" "}
+        numbers. All the expected operations you can do with a number can be
+        applyed to this type, some of the basic ones:
+      </p>
+      <code className="bg-gray-900 px-2 py-1 rounded text-purple-400 font-mono block whitespace-pre">
+        {" "}
+        * <span className="text-gray-400 text-sm"> // multiplication</span>
+        {"\n"} / <span className="text-gray-400 text-sm"> // division</span>
+        {"\n"} + <span className="text-gray-400 text-sm"> // addition</span>
+        {"\n"} - <span className="text-gray-400 text-sm"> // subtraction</span>
+      </code>
+      <p className="text-gray-400 text-lg sm:text-xl text-left my-6">
+        Besides regular numbers this type also includes special numeric values:
+      </p>
+      <code className="bg-gray-900 px-2 py-1 rounded text-purple-400 font-mono block whitespace-pre-wrap">
+        {" "}
+        NaN{" "}
+        <span className="text-gray-400 text-sm">
+          {" "}
+          // Not a Number - this is a computational error, the result of an
+          incorrect or undifine operation.
+        </span>
+        {"\n"} console.log(1/0){" "}
+        <span className="text-gray-400 text-sm"> // NaN</span>
+        {"\n"} Infinity -Infinit{" "}
+        <span className="text-gray-400 text-sm">
+          {" "}
+          // represents the mathematical infinity ∞ meaning a value that is
+          greater or less than any other number.
+        </span>
+        {"\n"} console.log(1/0){" "}
+        <span className="text-gray-400 text-sm"> // Infinity</span>
+        {"\n"} console.log()-1/0{" "}
+        <span className="text-gray-400 text-sm"> // -Infinity</span>
+      </code>
 
       {/* --- BigInt Section --- */}
-
+      {/* --- String Section --- */}
       {/* --- Boolean Section --- */}
-
       {/* --- null Section --- */}
-
       {/* --- undefined Section --- */}
     </ComponentBase>
   );
